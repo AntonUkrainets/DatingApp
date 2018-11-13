770a49db6023ada0818448d9d448bb2f3a5d11c4
Anton <anton.ukrainets@gmail.com>
2018-11-13 22:15:49 +0200

    .

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,7 +10,7 @@ namespace ElixirShop.API.Data
             : base(options) { }
 
         public DbSet<Value> Values { get; set; }
-
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
