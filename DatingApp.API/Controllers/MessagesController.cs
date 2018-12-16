ff89f22153276be7b01a011381b443c9f1bdca5e
Anton <anton.ukrainets@gmail.com>
2018-12-16 14:32:31 +0200

    fix display the photo in private messages

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -74,7 +74,9 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
             messageForCreationDto.SenderId = userId;
@@ -85,12 +87,12 @@ namespace DatingApp.API.Controllers
             
             var message = _mapper.Map<Message>(messageForCreationDto);   
 
-            _repo.Add(message);
+            _repo.Add(message);            
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
-            if(await _repo.SaveAll())
+            if(await _repo.SaveAll()) {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+            }
 
             throw new Exception("Creating the messages failed on save");
         }
